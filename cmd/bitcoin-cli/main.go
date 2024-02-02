f0ef718b8ad094b849d7d8b0fdfe7723b828cd32
Pierre Roullon <pierre.roullon@gmail.com>
2024-02-02 17:40:37 +0100

    feat: cli generate tx

--- cmd/bitcoin-cli/main.go
+++ cmd/bitcoin-cli/main.go
@@ -7,8 +7,14 @@ import (
 	"crypto/x509"
 	"fmt"
 	"os"
+	"sync"
 
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 	"github.com/spf13/cobra"
+
+	"github.com/proullon/bitcoin/node"
 )
 
 func generateKey(cmd *cobra.Command, args []string) error {
@@ -23,6 +29,38 @@ func generateKey(cmd *cobra.Command, args []string) error {
 	}
 
 	fmt.Printf("key: %x  (len=%d)", key, len(key))
+	return nil
+}
+
+type Notifee struct {
+	l sync.Cond
+}
+
+func (n *Notifee) HandlePeerFound(peerInfo peer.AddrInfo) {
+	fmt.Printf("Sending tx\n")
+	n.l.Broadcast()
+}
+
+// generateTx connects to bitcoin network and publish a transaction
+func generateTx(cmd *cobra.Command, args []string) error {
+
+	host, err := libp2p.New()
+	if err != nil {
+		return err
+	}
+
+	m := new(sync.Mutex)
+	notifee := &Notifee{
+		l: *sync.NewCond(m),
+	}
+
+	notifee.l.L.Lock()
+	err = mdns.NewMdnsService(host, node.DiscoveryNamespace, notifee).Start()
+	if err != nil {
+		return nil
+	}
+
+	notifee.l.Wait()
 	return nil
 }
 
@@ -46,6 +84,12 @@ func main() {
 		Use:   `generate-key`,
 		Short: `Generate an ECDSA key to be used on bitcoin network`,
 		RunE:  generateKey,
+	})
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   `generate-tx`,
+		Short: `Generate a transaction request on bitcoin network`,
+		RunE:  generateTx,
 	})
 
 	err := rootCmd.Execute()
