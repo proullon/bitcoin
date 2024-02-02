75b2cf52a17e4658db051a4a8193a2dd63f29420
Pierre Roullon <pierre.roullon@gmail.com>
2024-02-02 15:51:22 +0100

    feat: (node) init FSM

--- node/init.go
+++ node/init.go
@@ -11,6 +11,7 @@ import (
 
 	libp2p "github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/discovery/mdns"
 
@@ -27,58 +28,45 @@ const (
 	BlockTopic = "blocks"
 )
 
+type Status string
+
+const (
+	Ready      Status = "ready"
+	Failed     Status = "failed"
+	Retrieving Status = "retrieving blocks"
+	Waiting    Status = "waiting for transaction"
+	Building   Status = "building block"
+)
+
 type discoveryNotifee struct{}
 
 func (n *discoveryNotifee) HandlePeerFound(peerInfo peer.AddrInfo) {
 	fmt.Println("found peer", peerInfo.String())
 }
 
+type Node struct {
+	status Status
+
+	privateKey     *ecdsa.PrivateKey
+	bc             *blockchain.Blockchain
+	tentativeBlock *blockchain.Block
+
+	host    host.Host
+	txTopic *pubsub.Topic
+	bTopic  *pubsub.Topic
+	txSub   *pubsub.Subscription
+	bSub    *pubsub.Subscription
+}
+
 // Run initialise the node network with provided private key and disk working directory
 func Run() error {
-	ctx := context.Background()
 
-	host, err := libp2p.New(
-		libp2p.Ping(false),
-	)
+	node, err := NewNode()
 	if err != nil {
 		return err
 	}
-	defer host.Close()
 
-	fmt.Println(host.Addrs())
-
-	// PubSub
-	ps, err := pubsub.NewGossipSub(ctx, host)
-	if err != nil {
-		return fmt.Errorf("cannot start pubsub service: %s", err)
-	}
-	// join topics for transactions and blocks
-	txTopic, err := ps.Join(TxTopic)
-	if err != nil {
-		return fmt.Errorf("cannot join transaction topic: %s", err)
-	}
-	txSub, err := txTopic.Subscribe()
-	if err != nil {
-		return fmt.Errorf("cannot subscribe to transaction topic: %s", err)
-	}
-	bTopic, err := ps.Join(BlockTopic)
-	if err != nil {
-		return fmt.Errorf("cannot join transaction topic: %s", err)
-	}
-	bSub, err := bTopic.Subscribe()
-	if err != nil {
-		return fmt.Errorf("cannot subscribe to transaction topic: %s", err)
-	}
-
-	_ = txSub
-	_ = bSub
-
-	// Peer discovery
-	mdnsService := mdns.NewMdnsService(host, discoveryNamespace, &discoveryNotifee{})
-	err = mdnsService.Start()
-	if err != nil {
-		return fmt.Errorf("cannot start mdns service: %s", err)
-	}
+	node.Start()
 
 	// Exit properly on SIGTERM and SIGINT
 	sigCh := make(chan os.Signal, 1)
@@ -86,24 +74,64 @@ func Run() error {
 	<-sigCh
 
 	fmt.Printf("\nShutting down\n")
+	node.Stop()
 	return nil
 }
 
-type Node struct {
-	privateKey     *ecdsa.PrivateKey
-	bc             *blockchain.Blockchain
-	tentativeBlock *blockchain.Block
+func NewNode() (*Node, error) {
+	var err error
+	ctx := context.Background()
 
-	txTopic *pubsub.Topic
-	bTopic  *pubsub.Topic
-	txSub   *pubsub.Subscription
-	bSub    *pubsub.Subscription
+	node := &Node{status: Failed}
+
+	node.host, err = libp2p.New(
+		libp2p.Ping(false),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println(node.host.Addrs())
+
+	// PubSub
+	ps, err := pubsub.NewGossipSub(ctx, node.host)
+	if err != nil {
+		return nil, fmt.Errorf("cannot start pubsub service: %s", err)
+	}
+	// join topics for transactions and blocks
+	node.txTopic, err = ps.Join(TxTopic)
+	if err != nil {
+		return nil, fmt.Errorf("cannot join transaction topic: %s", err)
+	}
+	node.txSub, err = node.txTopic.Subscribe()
+	if err != nil {
+		return nil, fmt.Errorf("cannot subscribe to transaction topic: %s", err)
+	}
+	node.bTopic, err = ps.Join(BlockTopic)
+	if err != nil {
+		return nil, fmt.Errorf("cannot join transaction topic: %s", err)
+	}
+	node.bSub, err = node.bTopic.Subscribe()
+	if err != nil {
+		return nil, fmt.Errorf("cannot subscribe to transaction topic: %s", err)
+	}
+
+	// Peer discovery
+	mdnsService := mdns.NewMdnsService(node.host, discoveryNamespace, &discoveryNotifee{})
+	err = mdnsService.Start()
+	if err != nil {
+		return nil, fmt.Errorf("cannot start mdns service: %s", err)
+	}
+
+	node.status = Ready
+	return node, nil
 }
 
 func (n *Node) Start() {
 }
 
 func (n *Node) Stop() {
+	n.host.Close()
 }
 
 func (n *Node) run() {
